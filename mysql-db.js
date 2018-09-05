a2e58190e8a4df0705f6a17e19a8866b87a6a079
Adrian Acosta <aacosta@techlaunch>
2018-09-05 19:14:56 -0400

    database fixed

--- mysql-db.js
+++ mysql-db.js
@@ -4,7 +4,7 @@
 
 var mysql = require('mysql');
 var connection = mysql.createConnection({
-  host: 'localhost',
+  host: 'aacosta.techlaunch.io',
   user: 'root',
   password: 'open',
   database: 'todo'
